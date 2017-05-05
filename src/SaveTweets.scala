87695edb734dbcb1b7041f5c0c93aafd394dad99
tarunbakshi1406 <tarunbakshi1406@gmail.com>
2017-05-05 00:54:53 -0500

    Update SaveTweets.scala

--- src/SaveTweets.scala
+++ src/SaveTweets.scala
@@ -19,20 +19,21 @@ object SaveTweets {
   
     setupTwitter()
       val ssc = new StreamingContext("local[*]", "SaveTweets", Seconds(10000))
+    ///by changing the seconds here you will be able to get tweets over that period
     
    // setupLogging()
 
    
     val tweets = TwitterUtils.createStream(ssc, None)
 
-   
+   //we are getting only the tweets which are in english language and the tweets which contain the keyword sports that is verified
         val verified_users = tweets.filter(_.getLang()=="en").filter(_.getText().toLowerCase().contains("sports")).filter(_.getUser().isVerified()).map(status => (status.getUser().isVerified(),"::",status.getUser().getScreenName(),"::",status.getText(),"::",status.getUser().getLocation(),"::",status.getUser().getCreatedAt()))
    verified_users.foreachRDD(t => { 
       val a= t.map(a => a)  
       a.coalesce(1,true).saveAsTextFile("verified")
     }
     )
-    //statuses.print
+      //we are getting only the tweets which are in english language and the tweets which contain the keyword sports that is unverified
         val unverified_users = tweets.filter(_.getLang()=="en").filter(_.getText().toLowerCase().contains("sports")).filter(!_.getUser().isVerified()).map(status => (status.getUser().isVerified(),"::",status.getUser().getScreenName(),"::",status.getText(),"::",status.getUser().getLocation(),"::",status.getUser().getCreatedAt()))
 unverified_users.foreachRDD(t => { 
       val a= t.map(a => a)  
